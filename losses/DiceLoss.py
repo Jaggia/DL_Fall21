91a17f8139d39b2c912bf8b1efc94df620279f41
anadi <anadi.jaggia@gmail.com>
2021-12-10 14:19:47 -0500

    diceloss final m8

--- losses/DiceLoss.py
+++ losses/DiceLoss.py
@@ -29,42 +29,26 @@ class DiceLoss(nn.Module):
         # comment out for diff implementations
         return self.kornia(inputs, targets)
 
-    def dice_loss(self, true, logits, eps=1e-7):
-        # https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
+    def kornia(self, input, target):
+        # https://kornia.readthedocs.io/en/v0.1.2/_modules/torchgeometry/losses/dice.html
         """
-        Computes the Sørensen–Dice loss.
-        Note that PyTorch optimizers minimize a loss. In this
-        case, we would like to maximize the dice loss so we
-        return the negated dice loss.
-        Args:
-            true: a tensor of shape [B, 1, H, W].
-            logits: a tensor of shape [B, C, H, W]. Corresponds to
-                the raw output or logits of the model.
-            eps: added to the denominator for numerical stability.
-        Returns:
-            dice_loss: the Sørensen–Dice loss.
+        Shape:
+        - Input: :math:`(N, C, H, W)` where C = number of classes.
+        - Target: :math:`(N, H, W)` where each value is
+          :math:`0 ≤ targets[i] ≤ C−1`.
         """
-        # print(true.shape)
-        # print(logits.shape)
-        true = true.unsqueeze(1).unsqueeze(-1).unsqueeze(-1)
-        logits = logits.unsqueeze(-1).unsqueeze(-1)
-        num_classes = logits.shape[1]
-        if num_classes == 1:
-            true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
-            true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-            true_1_hot_f = true_1_hot[:, 0:1, :, :]
-            true_1_hot_s = true_1_hot[:, 1:2, :, :]
-            true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
-            pos_prob = torch.sigmoid(logits)
-            neg_prob = 1 - pos_prob
-            probas = torch.cat([pos_prob, neg_prob], dim=1)
-        else:
-            true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
-            true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-            probas = F.softmax(logits, dim=1)
-        true_1_hot = true_1_hot.type(logits.type())
-        dims = (0,) + tuple(range(2, true.ndimension()))
-        intersection = torch.sum(probas * true_1_hot, dims)
-        cardinality = torch.sum(probas + true_1_hot, dims)
-        dice_loss = (2. * intersection / (cardinality + eps)).mean()
-        return (1 - dice_loss)
+        input = input.unsqueeze(-1).unsqueeze(-1)
+        target = target.unsqueeze(-1).unsqueeze(-1)
+        # compute softmax over the classes axis
+        input_soft = F.softmax(input, dim=1)
+
+        # create the labels one hot tensor
+        target_one_hot = F.one_hot(target, num_classes=input.shape[1])
+
+        # compute the actual dice score
+        dims = (1, 2, 3)
+        intersection = torch.sum(input_soft * target_one_hot, dims)
+        cardinality = torch.sum(input_soft + target_one_hot, dims)
+
+        dice_score = 2. * intersection / (cardinality + self.eps)
+        return torch.mean(1. - dice_score)
