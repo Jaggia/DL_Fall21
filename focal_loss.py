784aa26aecbf6bb7c36753ee1ea55c47221eabef
Nick Torvik <nitorvik@gmail.com>
2021-12-15 20:30:25 -0500

    focal loss and weight calcs

---
+++ focal_loss.py
@@ -0,0 +1,55 @@
+"""
+SOURCE
+# https://github.com/DingKe/pytorch_workplace/blob/master/focalloss/loss.py
+
+"""
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+
+def one_hot(index, classes):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    index = index.to(device)
+    size = index.size() + (classes,)
+    view = index.size() + (1,)
+
+    mask = torch.Tensor(*size).fill_(0).to(device)
+    index = index.view(*view)
+    ones = 1.
+
+    if isinstance(index, Variable):
+        ones = Variable(torch.Tensor(index.size()).fill_(1)).to(device)
+        mask = Variable(mask, volatile=index.volatile).to(device)
+
+    return mask.scatter_(1, index, ones)
+
+
+class FocalLoss(nn.Module):
+
+    def __init__(self, gamma=0, eps=1e-7, weight= None):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.eps = eps
+        self.weight = weight
+
+    def forward(self, input, target):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        input = input.to(device)
+        target = target.to(device)
+        y = one_hot(target, input.size(-1))
+        logit = F.softmax(input, dim=-1).to(device)
+        logit = logit.clamp(self.eps, 1. - self.eps)
+
+        loss = -1 * y * torch.log(logit) # cross entropy
+
+        if self.weight == None: # add for handling weights
+            loss = loss * (1 - logit) ** self.gamma
+        else:
+            loss = loss * (1 - logit) ** self.gamma * self.weight # focal loss
+
+        return loss.sum()
--- weights.py
+++ weights.py
@@ -1,3 +1,17 @@
+"""
+Impact of Various Techniques on Minority Subject Area News Text Classification
+CS7643
+
+Calculate weights to be passed into loss functions.
+
+REFERENCES
+# https://androidkt.com/how-to-use-class-weight-in-crossentropyloss-for-an-imbalanced-dataset/
+# https://medium.com/gumgum-tech/handling-class-imbalance-by-introducing-sample-weighting-in-the-loss-function-3bdebd8203b4        
+# https://github.com/vandit15/Class-balanced-loss-pytorch/blob/master/class_balanced_loss.py
+# https://discuss.pytorch.org/t/per-class-and-per-sample-weighting/25530/4
+# https://pytorch.org/docs/stable/generated/torch.sort.html
+
+"""
 
 import torch
 import numpy as np
@@ -6,28 +20,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-
-# https://androidkt.com/how-to-use-class-weight-in-crossentropyloss-for-an-imbalanced-dataset/
-
 def weightweight(cls_num_list, type= "ins", beta=0.9999):
     if type == "classbal":
-        # https://github.com/vandit15/Class-balanced-loss-pytorch/blob/master/class_balanced_loss.py
         weights = (1-beta)/(1-np.power(beta,cls_num_list))
     elif type == "ins":
         weights = 1.0/np.array(cls_num_list)
     elif type == "isns":
         weights = 1.0/np.sqrt(cls_num_list)
     elif type == "uniform":
-        # https://discuss.pytorch.org/t/per-class-and-per-sample-weighting/25530/4
-        # https://pytorch.org/docs/stable/generated/torch.sort.html
         weights = torch.empty(41).uniform_(0, 1)
         weights, indices = torch.sort(weights)
     elif type == "simple":
         weights = 1 - cls_num_list/np.sum(cls_num_list)
-    
+
     weights = torch.as_tensor(weights)
     weights = weights.float()
-
-    # print(weights)
 
     return weights
